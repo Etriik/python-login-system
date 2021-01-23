e870d856d03fb6ecf0f3fbb423800526ccf5c1af
Etriik <76824415+Etriik@users.noreply.github.com>
2021-01-23 22:27:16 +0100

    Add files via upload

---
+++ Login.py
@@ -0,0 +1,16 @@
+import time
+
+
+name = input("Username: ")
+if name == "Admin":
+    pass
+else:
+    sys.exit(0)
+password = input("Password: ")
+if password == "admin":
+    pass
+else:
+    sys.exit(0)
+if "pass":
+    print("Verified")
+time.sleep(2.0)
