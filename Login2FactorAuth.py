d959c27c18c16395ec6979f2c462d132c1d5157b
Etrik <76824415+Etriik@users.noreply.github.com>
2021-01-25 12:47:25 +0100

    Add files via upload
    The same python log in system but with 2 factor authentication.

---
+++ Login2FactorAuth.py
@@ -0,0 +1,27 @@
+import time
+
+
+name = input("Username: ")
+if name == "YourName":
+    pass
+else:
+    sys.exit(0)
+password = input("Password: ")
+if password == "YourPassword":
+    pass
+else:
+    sys.exit(0)
+firstquestion = input("What is your favorite color? ")
+if firstquestion == "YourAnswer":
+    pass
+else:
+    sys.exit(0)
+secondquestion = input("What was your first pets name? ")
+if secondquestion == "YourAnswer":
+    pass
+else:
+    sys.exit(0)
+thirdquestion = input("What is your favorite game? ")
+if thirdquestion == "YourAnswer":
+    print("Verified")
+time.sleep(1.5)
